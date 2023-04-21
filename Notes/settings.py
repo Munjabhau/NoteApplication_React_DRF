3d57ef1e7c1116765629a5bd5535794b8a5c716f
Chaitanya <Kale.Chaitanya0304@gmail.com>
2023-04-22 01:02:16 +0530

    fix error

--- Notes/settings.py
+++ Notes/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m1(^-&%%pf05a2op5&k8(*%(@&a%c05#22c27))q%ci&bhpa*_'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
